79283ebb3e84e0f88ffc01407693dcbf3ff0a75a
myotz <myothantzin.mtz412@gmail.com>
2025-11-19 01:36:06 -0800

    Made a little update. no major change

--- GameManager/src/blackjack/model/Player.java
+++ GameManager/src/blackjack/model/Player.java
@@ -4,70 +4,77 @@ public class Player {
     protected final String name;
     protected int money;
     protected int bet;
-    protected final Hand hand;
+    protected final Hand hand = new Hand();
 
-    public Player(String name, int initialMoney){
+    public Player(String name, int initialMoney) {
         this.name = name;
         this.money = initialMoney;
-        this.hand = new Hand();
+        // this.hand = new Hand();
     }
 
-    public void add(Card card){
+    public void add(Card card) {
         hand.add(card);
     }
 
-    public void clear(){
-        hand.getCards().clear();
+    public void clear() {
+        hand.clear();
         bet = 0;
     }
 
-    public int getValue(){
+    public int getValue() {
         return hand.value();
     }
 
-    public boolean isBust(){
+    public boolean isBust() {
         return hand.isBust();
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void playBet(int amount){
+    public Hand getHand() {
+        return hand;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void playBet(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Bet must be positive");
+        }
         if (amount > money) {
             throw new IllegalArgumentException("Bet exceeds available money");
         }
-        this.bet = amount;
+        this.bet += amount;
         this.money -= amount;
     }
 
-    public int winBet(){
+    public int winBet() {
         int winnings = bet * 2;
         this.money += winnings;
         this.bet = 0;
         return winnings;
     }
 
-    public int pushBet(){
+    public int pushBet() {
         this.money += bet;
         this.bet = 0;
         return bet;
     }
 
-    public void loseBet(){
+    public void loseBet() {
         this.bet = 0;
-    }  
-
-    public int getMoney(){
-        return money;
-    }
-
-    public Hand getHand(){
-        return hand;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return name + " (Money: $" + money + ", Bet: $" + bet + ", Hand Value: " + hand.value() + ")";
     }
 
